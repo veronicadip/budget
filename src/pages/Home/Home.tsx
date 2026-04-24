9da332304eea36f6bbbabd4073f8e77f7bf5b007
Maria Veronica Dip <dipmveronica@gmail.com>
2026-04-24 00:44:39 -0300

    added basic logic to get started

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useState, type KeyboardEvent } from "react";
+
 export const Home: React.FC = () => {
+    const [income, setIncome] = useState(0);
+    const [expenses, setExpenses] = useState(0);
+    const [totalBalance, setTotalBalance] = useState(0);
+
+    const handleIncomeKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            setIncome(income + Number(event.currentTarget.value));
+        }
+    }
+
+    const handleExpensesKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            setExpenses(expenses + Number(event.currentTarget.value));
+        }
+    }
+
+    useEffect(() => {
+        setTotalBalance(income - expenses);
+    }, [income, expenses]);
+    
     return(
-        <>Home Page</>
+        <>
+            <div>Income: {income}</div>
+            <div>Expenses: {expenses}</div>
+            <div>Total Balance: {totalBalance}</div>
+
+            <input type="number" placeholder="add an INCOME" onKeyDown={(e) => handleIncomeKeyDown(e)} />
+            <input type="number" placeholder="add an EXPENSE" onKeyDown={(e) => handleExpensesKeyDown(e)} />
+        </>
     )
 };
